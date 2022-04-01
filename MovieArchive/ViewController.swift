012f22d7199c468cb0ba1355111309658b658c8c
MehmetBilirr <mehmetbilir@aol.com>
2022-04-01 21:53:30 +0300

    fetch data

--- MovieArchive/ViewController.swift
+++ MovieArchive/ViewController.swift
@@ -6,15 +6,20 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var nameArray = [String]()
+    var idArray = [UUID]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Add button 
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        getData()
     }
     
     
@@ -22,6 +27,35 @@ class ViewController: UIViewController {
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
         
     }
+    
+    
+    
+    @objc func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Movies")
+        fetchRequest.returnsObjectsAsFaults = false
+        do {
+            
+            let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        
+                        if let name = result.value(forKey: "name") as? String{
+                            self.nameArray.append(name)
+                        }
+                        if let id = result.value(forKey: "id") as? UUID{
+                            self.idArray.append(id)
+                        }
+                        self.tableView.reloadData()
+                        
+                    }
+                }
+        }catch{
+            print("error")
+        }
+        }
+    
             
 
 
