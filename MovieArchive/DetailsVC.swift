87359825514a7f5024ec66f873f550c5ee5b6dca
MehmetBilirr <mehmetbilir@aol.com>
2022-04-01 21:23:36 +0300

    select image from library

--- MovieArchive/DetailsVC.swift
+++ MovieArchive/DetailsVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var movieName: UITextField!
     @IBOutlet weak var imdbRating: UITextField!
@@ -18,8 +18,34 @@ class DetailsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //Hide keyboard
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        // Image recognizer
+        imageView.isUserInteractionEnabled = true
+        let gestureImageRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(gestureImageRecognizer)
         
+    }
+    
+    @objc func hideKeyboard(){
+        
+        view.endEditing(true)
+        
+    }
+    
+    @objc func selectImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
     }
     
     @IBAction func saveButton(_ sender: Any) {
--- MovieArchive/ViewController.swift
+++ MovieArchive/ViewController.swift
@@ -12,8 +12,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Add button 
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
     }
+    
+    
+    @objc func addButtonClicked(){
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+        
+    }
+            
 
 
 }
