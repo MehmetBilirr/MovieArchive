b728091769956dbd2adfb36f025ed4c88847ec1d
MehmetBilirr <mehmetbilir@aol.com>
2022-04-01 21:08:46 +0300

    defines storyboard and xcdatamodeld

---
+++ MovieArchive/DetailsVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsVC.swift
+//  MovieArchive
+//
+//  Created by Mehmet Bilir on 1.04.2022.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var movieName: UITextField!
+    @IBOutlet weak var imdbRating: UITextField!
+    @IBOutlet weak var year: UITextField!
+    @IBOutlet weak var director: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+   
+
+}
